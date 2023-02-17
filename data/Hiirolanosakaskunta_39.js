15b7de35508d6648367f972922fd79593c55257a
KS-KTKJP <125342444+KS-KTKJP@users.noreply.github.com>
2023-02-17 13:36:40 +0200

    Add files via upload

---
+++ data/Hiirolanosakaskunta_39.js
@@ -0,0 +1,1 @@
+var json_Hiirolanosakaskunta_39 = {"type":"FeatureCollection","name":"Hiirolanosakaskunta_39","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
